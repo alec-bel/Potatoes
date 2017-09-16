fc84a5071e706ba79908528af1dc047cb41af70a
Vishnu Reddy <vishnureddy17@gmail.com>
2017-09-16 18:12:00 -0400

    Updated manifest to include Place API key. Updared build.gradle to include places api.

--- app/src/main/java/com/example/abeljan/potatoes/MainActivity.java
+++ app/src/main/java/com/example/abeljan/potatoes/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+//import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Places;
+//import com.google.android.gms.location.places.PlaceDetectionClient;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
