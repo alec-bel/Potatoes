0adf804eed72e130eb67e30eeebb36fab3b5043f
Ivan Mondragon <imondrag@umich.edu>
2017-09-16 17:32:35 -0400

    Got the map fragment working!

--- app/src/main/java/com/example/abeljan/potatoes/MainActivity.java
+++ app/src/main/java/com/example/abeljan/potatoes/MainActivity.java
@@ -1,28 +1,35 @@
 package com.example.abeljan.potatoes;
 
+
+
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     Button map_btn;
     Button scoreboard_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        map_btn = (Button) findViewById(R.id.map_btn);
-        map_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(i);
-            }
-        });
+        MapFragment mapFrag = MapFragment.newInstance();
+        mapFrag.getMapAsync(this);
 
         scoreboard_btn = (Button) findViewById(R.id.scoreboard_btn);
         scoreboard_btn.setOnClickListener(new View.OnClickListener(){
@@ -31,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng UoM = new LatLng(42.276938, -83.738205); // Block 'M'
+        map.addMarker(new MarkerOptions().position(UoM).title("University of Michigan"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(UoM));
     }
 }
