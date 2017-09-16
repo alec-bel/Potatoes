6676f74e8712d4a66171f938ecee15e13d519db5
Ivan Mondragon <imondrag@umich.edu>
2017-09-16 13:35:41 -0400

    Add working map

--- app/src/main/java/com/example/abeljan/potatoes/MainActivity.java
+++ app/src/main/java/com/example/abeljan/potatoes/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.abeljan.potatoes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button map_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map_btn = (Button) findViewById(R.id.map_btn);
+        map_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
