db6d53842fec917e2b51319563a68b459d109d36
Vishnu Reddy <vishnureddy17@gmail.com>
2017-09-16 18:25:25 -0400

    Updated manifest to include Place API key. Updared build.gradle to include places api.

--- app/src/main/java/com/example/abeljan/potatoes/MapsActivity.java
+++ app/src/main/java/com/example/abeljan/potatoes/MapsActivity.java
@@ -9,6 +9,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.places.Places;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
